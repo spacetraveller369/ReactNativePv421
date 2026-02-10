75bc3060bff86e750470c7c7a6034deab41c0dd0
Ilono4ka <movlyanovai@gmail.com>
2026-02-10 20:59:39 +0100

    new styles
    Refactor CalcStyle by removing unused styles and updating existing ones for better layout.

--- src/pages/calc/css/CalcStyle.ts
+++ src/pages/calc/css/CalcStyle.ts
@@ -2,47 +2,41 @@ import { StyleSheet } from "react-native";
 import { textColor } from "../../../features/values/colors";
 
 const CalcStyle = StyleSheet.create({
+  
    calc: {
     flex: 1,
-    width: "100%",
-    backgroundColor: "#222",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
+    padding: 10,              
+    backgroundColor: "#1C1C1C", 
+    justifyContent: "space-between", 
    },
-
-   expression: {
-    flex: 1,
-    color: "#bbb",
-    textAlign: "right",
-    marginTop: 10,
-   },
-
    result: {
-    flex: 2,
-    color: textColor,
-    // fontSize: 50,
-    textAlign: "right",
+    color: textColor,         
+    textAlign: "right",       
+    fontWeight: "300",       
+    paddingRight: 10,
+    marginBottom: 20,
    },
-
+   expression: {
+    color: "#777",            
+    textAlign: "right",       
+    fontSize: 20,
+    marginBottom: 5,
+   },
    memoryRow: {
     flex: 1,
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
    },
-
    buttonRow: {
     flex: 2,
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
    },
-
    memoryButton: {
     color: "#bbb",
    },
-
    // Landscape
    containerResExpMem: {
       flex: 2,
@@ -50,7 +44,6 @@ const CalcStyle = StyleSheet.create({
       flexDirection: "row",
       justifyContent: "space-between",
    },
-   
    containerExpMem: {
       flex: 2,
       display: "flex",
