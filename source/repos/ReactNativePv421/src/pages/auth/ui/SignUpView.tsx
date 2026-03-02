87d084d2745f6c821574be57e8c0cf8a777ddbcd
Ilono4ka <movlyanovai@gmail.com>
2026-03-02 21:37:21 +0200

    Update SignUpView.tsx

--- source/repos/ReactNativePv421/src/pages/auth/ui/SignUpView.tsx
+++ source/repos/ReactNativePv421/src/pages/auth/ui/SignUpView.tsx
@@ -11,7 +11,7 @@ interface IUserFormData {
     birthdate: Date|null,
     password: string,
     repeat: string,
-    phone: string
+    phone: string,
     city: string
 };
 
@@ -22,7 +22,7 @@ const emptyUserFormData = {
     password: "",
     repeat: "",
     phone: "",
-    city: "",
+    city: ""
 };
 const testUserFormData = {
     name: "Досвічений Користувач",
